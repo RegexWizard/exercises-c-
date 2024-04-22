ddb073780eac64f0e09814cf12854915c48f7b8a
Regex Wizard <161946836+RegexWizard@users.noreply.github.com>
2024-04-22 16:34:41 +0700

    Add files via upload

---
+++ exercises2.cpp
@@ -0,0 +1,12 @@
+// ToDo: Write a program that switch Fahrenheit to Celcius
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "Insert a value Degree of Fahrenheit to switch them into Celcius: ";
+    double Value;
+    cin >> Value;
+    cout << "The " << Value << "*F is... " << (Value - 273) << "*C";
+}
